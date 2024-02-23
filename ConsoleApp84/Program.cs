8c38c0820c0ecd9cae7ecdb1fafd840850f27176
ygursu <yasemingrs@gmail.com>
2024-02-23 23:55:44 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp84/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedikdortgenprizmasininhacmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Dikdörtgenler prizmasının a kenarını giriniz");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dikdörtgenler prizmasının b kenarını giriniz");
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dikdörtgenler prizmasının c kenarını giriniz");
+            int c = Int32.Parse(Console.ReadLine());
+
+            int hacim = a * b * c;
+
+            Console.WriteLine("Dikdörtgenler prizmasının hacmi={0}", hacim);
+            Console.ReadLine();
+        }
+    }
+}
